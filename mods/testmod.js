7804c69af4a7521dd559628b6e71d2684b8c22b8
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-10-24 14:33:12 -0500

    Update testmod.js

--- mods/testmod.js
+++ mods/testmod.js
@@ -1,1 +1,19 @@
 console.log("lol");
+
+element.cream = {
+    color: "#f5f2e1",
+    behavior: behaviors.STICKY,
+    category: "food",
+    viscosity: 10000,
+    state: "liquid",
+    density: 1000
+}
+
+element.apple = {
+    color: "#ed2d40",
+    behavior: behaviors.STURDYPOWDER,
+    category: "food",
+    viscosity: 1000,
+    state: "solid",
+    density: 1000
+}
