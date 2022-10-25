704ba13b87d154d2e506fc479a2df51d9c7e967b
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-10-25 09:05:44 -0500

    Create spawntest.js

---
+++ spawntest.js
@@ -0,0 +1,13 @@
+elements.sand_spawn = {
+  color: "#ffffff",
+  behavior:
+  [
+    "CR:sand|XX|XX",
+    "XX|DL|XX",
+    "XX|XX|XX",
+  ],
+  category: "special",
+  viscosity: 100000,
+  state: "solid",
+  density: 720,
+}
