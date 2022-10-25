acc019e6c6abe8f2d706cf8f13575b519c02d156
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-10-25 09:10:25 -0500

    Update spawntest.js

--- spawntest.js
+++ spawntest.js
@@ -11,3 +11,32 @@ elements.sand_spawn = {
   state: "solid",
   density: 720,
 }
+
+
+elements.card_spawn = {
+  color: "#ffffff",
+  behavior:
+  [
+    "XX|CR:dig_spawn|XX",
+    "CR:dig_spawn|DL|CR:dig_spawn",
+    "XX|CR:dig_spawn|XX",
+  ],
+  category: "special",
+  viscosity: 100000,
+  state: "solid",
+  density: 720,
+}
+
+elements.dig_spawn = {
+  color: "#ffffff",
+  behavior:
+  [
+    "CR:card_spawn|XX|CR:card_spawn",
+    "XX|DL|XX",
+    "CR:card_spawn|XX|CR:card_spawn",
+  ],
+  category: "special",
+  viscosity: 100000,
+  state: "solid",
+  density: 720,
+}
