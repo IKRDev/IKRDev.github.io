497ab0ce8aaeccb700af9fae78e75cc49ef8c7cd
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-10-24 20:41:49 -0500

    Update extra_foodstuffs.js

--- mods/extra_foodstuffs.js
+++ mods/extra_foodstuffs.js
@@ -4,16 +4,61 @@ elements.cream = {
     color: "#f5f2e1",
     behavior: behaviors.LIQUID,
     category: "food",
-    viscosity: 25,
+    viscosity: 35,
     state: "liquid",
     density: 0,
 };
 
+// fruit
+
 elements.apple = {
     color: "#ed2d40",
-    behavior: behaviors.POWDER,
+    behavior: [
+                    "XX|ST:vine|XX",
+                    "XX|XX|XX",
+                    "M2|M1|M2",
+                ],
     category: "food",
     viscosity: 0,
     state: "solid",
     density: 0,
+    breakInto: "apple_juice"
+};
+
+elements.orange = {
+    color: "#ed2d40",
+    behavior: [
+                    "XX|ST:vine|XX",
+                    "XX|XX|XX",
+                    "M2|M1|M2",
+                ],
+    category: "food",
+    viscosity: 0,
+    state: "solid",
+    density: 0,
+    breakInto: "orange_juice"
+};
+
+elements.apple_juice: {
+    color: "#ffe46b",
+    behavior: behaviors.LIQUID,
+    tempHigh: 160,
+    stateHigh: ["steam","sugar"],
+    tempLow: -10,
+    category: "liquids",
+    state: "liquid",
+    density: 1054   
+    stain: 0.05,
+};
+
+elements.orange_juice: {
+    color: "#fcb72b",
+    behavior: behaviors.LIQUID,
+    tempHigh: 160,
+    stateHigh: ["steam"],
+    tempLow: -10,
+    category: "liquids",
+    state: "liquid",
+    density: 1054   
+    stain: 0.05,
 };
