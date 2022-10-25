d728d9cd4949d66ee3756f2995c4b4969e13ca84
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-10-25 11:38:02 -0500

    Create machinery.js

---
+++ mods/machinery.js
@@ -0,0 +1,20 @@
+// fill from (fx, fy) to (tx, ty) with (elm)
+function fill(fx, fy, tx, ty, elm) {
+  for(var x=Math.min(fx, tx); x<Math.max(fx, tx); x++) {
+    for(var y=Math.max(fy, ty); y>Math.min(fy, ty); y--) {
+      pixelMap[x][y].element = elm;
+    }
+  }
+}
+
+elements.mixer = {
+  color:"#fff3db",
+  behavior:behaviors.SOLID,
+  category:"machines",
+  state:"solid",
+  temp:0
+}
+
+behaviors.mixer.tick = function(pixel) {
+  fill(pixel.x - Math.floor(pixel.temp / 2), pixel.y + Math.floor(pixel.temp / 2), pixel.x + Math.floor(pixel.temp / 2), pixel.y - Math.floor(pixel.temp / 2), "mix");
+}
